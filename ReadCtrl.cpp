9cec3b2e92aee9143c2e3dcb02214ac5e4660a9e
Bruce Chou <bchowder@gmail.com>
2012-12-03 17:57:07 -0500

    working version + input sample
    Note: input file cannot be modified in unix, otherwise the eof gets
    removed and seg fault happens!

--- ReadCtrl.cpp
+++ ReadCtrl.cpp
@@ -1,55 +1,85 @@
 /******************************************************************************\
 	This cpp function will parse an input file and grab the parameters from
 	said file and store them in the Simulation class structure. 
+	At the moment, however, it is a standalone program for debugging purpose
 \******************************************************************************/
 #include <iostream>
 using std::cout;
 using std::endl;
-#include <cctype>
 #include <fstream>
 using std::ifstream;
 #include <cstring>
 #include <string>
-#include <functional>
-#include <algorithm>
-const int MAX_CHARS = 512;
-const int MAX_TOKENS = 2;
-const char* const DELIMITER = ": ";
 #include <sstream>
 using namespace std;
+#include <vector>
+#include <cstddef>
 
-unsigned field_index(const char* s )
+//The following structure, split, is used to split the input string into field and data substrings
+//It is a standard structure obtained from the cpp resources library online 
+struct split
 {
-	//Here we list all possible fields. This can be expanded in the future
-	//original code using "find" didn't compile because "find" was not found, therefore using if's
+  enum empties_t { empties_ok, no_empties };
+};
+
+template <typename Container>
+Container& split(
+  Container&                            result,
+  const typename Container::value_type& s,
+  const typename Container::value_type& delimiters,
+  split::empties_t                      empties = split::empties_ok )
+{
+  result.clear();
+  size_t current;
+  size_t next = -1;
+  do
+  {
+    if (empties == split::no_empties)
+    {
+      next = s.find_first_not_of( delimiters, next + 1 );
+      if (next == Container::value_type::npos) break;
+      next -= 1;
+    }
+    current = next + 1;
+    next = s.find_first_of( delimiters, current );
+    result.push_back( s.substr( current, next - current ) );
+  }
+  while (next != Container::value_type::npos);
+  return result;
+}
+
+//Here we list all possible fields. This can be expanded in the future by appending additional fields.
+//This was originally supposed to be done using "find" function, but if statements will suffice.
+unsigned field_index(string& s )
+{
 	if (s == "name")		return 1;
 	if (s == "dir")			return 2;
 	if (s == "maxRes")		return 3;
 	if (s == "minRes")		return 4;
 	if (s == "blockSize")	return 5;
 	if (s == "cell")		return 6;		
-	if (s == "maxT")		return 7;	
+	if (s == "maxT")		return 7;
+	if (s == "freq")		return 8;
 	return 0;
 }
+
 int main()
 {
-	// create a file-reading object
-	ifstream input_file;
-	input_file.open("input.txt");	// open a file
-	if (!input_file.good())
-		return 1;	// exit if file not found
-	
-	// read each line of the file
-	while (!input_file.eof())
+	// the input file is read in line by line into string "line" until eof
+	string line;
+	ifstream inputFile ("input.txt");
+	if (inputFile.is_open())
 	{
-			// read each line as "field : data" and store in vector field_data 
-			char line[MAX_CHARS];
-			input_file.getline(line,MAX_CHARS);
-			const char* field_data[MAX_TOKENS] = {0}; // initialize to 0
-			field_data[0] = strtok(line, DELIMITER);
-			field_data[1] = strtok(0, "\n");
-			int data = 0;
-			switch (field_index( field_data[0] ))
+		while (!inputFile.eof())
+		{
+			vector <string> field_data;			
+			getline (inputFile, line);
+			//here we split each line into field and data by ":" & " ".
+			split( field_data, line, ": ", split::no_empties );			
+			// cout << field_data[0] << endl; //debug, normally commented out
+
+			// the following basically performs look up function to determine how to treat the data in each field
+			switch (field_index(field_data[0]))
 			{
 				case 0: //no match
 				{
@@ -58,45 +88,61 @@ int main()
 				}
 				case 1: //name, in string format
 				{
-					cout << field_data[0] << " = " << field_data[1] << "\n";
+					cout << field_data[0] << " = " << field_data[1] << endl;
 					break;
 				}
 				case 2: //directory, in string format
 				{
-					cout << field_data[0] << " = " << field_data[1] << "\n";
+					//we need to add the directory back because the split function splits ":"
+					field_data[1].append(":");
+					field_data[1].append(field_data[2]);	
+					cout << field_data[0] << " = " << field_data[1] << endl;
 					break;
 				}
 				case 3: //maximum resolution, in integer format
 				{
-					stringstream(field_data[1]) >> data;
-					cout << field_data[0] << " = " << data << "\n";
+					int maxRes;
+					stringstream(field_data[1]) >> maxRes;
+					cout << field_data[0] << " = " << maxRes << endl;
 					break;
 				}
 				case 4: //minimum resolution, in integer format
 				{
-					stringstream(field_data[1]) >> data;
-					cout << field_data[0] << " = " << data << "\n";
+					int minRes;
+					stringstream(field_data[1]) >> minRes;
+					cout << field_data[0] << " = " << minRes << endl;
 					break;
 				}
 				case 5: //block size, in integer format
 				{
-					stringstream(field_data[1]) >> data;
-					cout << field_data[0] << " = " << data << "\n";
+					int blockSize;
+					stringstream(field_data[1]) >> blockSize;
+					cout << field_data[0] << " = " << blockSize << endl;
 					break;
 				}
 				case 6: //cell dimension, with x & y steps in (x,y) vector format, x & y are integers
 				{
-					
-					cout << field_data[0] << " = " << field_data[1] << "\n";
+					cout << field_data[0] << " = " << field_data[1] << endl;
 					break;
 				}				
 				case 7: //maximum time steps, in integer format
 				{
-					stringstream(field_data[1]) >> data;
-					cout << field_data[0] << " = " << data << "\n";
+					int maxT;
+					stringstream(field_data[1]) >> maxT;
+					cout << field_data[0] << " = " << maxT << endl;
 					break;					
-				}
+				}		
+				case 8: //maximum time steps, in integer format
+				{
+					double freq;
+					stringstream(field_data[1]) >> freq;
+					cout << field_data[0] << " = " << freq << endl;
+					break;					
+				}				
 			}
-			
+		}
+		inputFile.close();
 	}
+	else cout << "Can't open input file.\n";
+	return 0;
 }
